fb59e044ce686eca96378b46a00bccc82665ddd2
yoodz <904201050@qq.com>
2026-02-17 00:09:48 +0800

    feat: allow additional local origins for CORS in development environment

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ module.exports = async function (app, opts) {
   app.register(fastifyCors, {
     origin: (origin, cb) => {
       // 开发调试阶段或者其他项目的开发环境，直接允许
-      if (!origin || process.env.NODE_ENV === 'development' || origin?.includes('localhost') || origin?.includes('afunny.top') || origin?.includes('goagix.com')) {
+      if (!origin || process.env.NODE_ENV === 'development' || origin?.includes('localhost') || origin?.includes('afunny.top') || origin?.includes('goagix.com') || origin?.includes('192.168.31') || origin?.includes('127.0.0.1')) {
         cb(null, true);
         return;
       }
